89cddac2f56fa00f289134c3518da2081de27359
Victor de Oliveira <oliveira.victor.51200@gmail.com>
2022-05-19 21:25:37 -0300

    Model Admin feito

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Admin, type: :model do
+  describe '#valid?' do
+    context 'e-mail domain' do
+      it 'is not @sistemadefrete.com.br' do
+        a = Admin.new(name: 'Fred', email: 'frederico_marmelo@domínioerrado.com.br',
+                      password: 'password')
+        r = a.valid?
+
+        expect(r).to eq false
+      end
+
+      it 'is @sistemadefrete.com.br' do
+        a = Admin.new(name: 'Fred', email: 'frederico_marmelo@sistemadefrete.com.br',
+                      password: 'password')
+        r = a.valid?
+
+        expect(r).to eq true
+      end
+    end
+  end
+end
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -4,7 +4,25 @@ describe 'Usuário acessa a tela inicial' do
   it 'e a vê' do
     visit root_path
 
+    expect(page).to have_content 'Sistema de frete'
     expect(page).to have_content 'Bem vindo'
-    expect(page).to have_link 'Clique aqui para começar'
+    expect(page).to have_link 'Admin'
+    expect(page).to have_link 'Usuário'
+  end
+
+  it 'e acessa a pagina de sign-in do Admin' do
+    visit root_path
+    click_on 'Admin'
+
+    expect(page).to have_field "E-mail"
+    expect(page).to have_field "Senha"
+  end
+
+  it 'e acessa a pagina de sign in do Usuário' do
+    visit root_path
+    click_on 'Usuário'
+
+    expect(page).to have_field "E-mail"
+    expect(page).to have_field "Senha"
   end
 end
