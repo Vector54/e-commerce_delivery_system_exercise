17920213e1a8ad09ac88b4c746ea7d54025b5bf8
Victor de Oliveira <oliveira.victor.51200@gmail.com>
2022-07-10 10:16:33 -0300

    refactor: Adiciona veículos e linhas de prazo no seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,7 @@ confirmed_admin = Admin.create!(email: 'ligia_confirmed@sistemadefrete.com.br', 
 print ". DONE\n".green
 
 puts 'creating => shipping_companies'.bg_green
+puts 'OBS: two are inactive and two are active'.red.bg_gray
 
 inactive_shipping_company1 = ShippingCompany.create!(name: 'Frete do Seu João',
                                                      corporate_name: 'FRETE DO SEU JOÃO LTDA',
@@ -100,4 +101,74 @@ print '.'.green
 priceline8 = PriceLine.create!(minimum_volume: 20, maximum_volume: 60, minimum_weight: 61,
                                maximum_weight: 180, value: 250,
                                price_table: PriceTable.find_by(shipping_company: active_shipping_company2))
+print ". DONE\n".green
+
+puts 'creating => delivery_time_lines'.bg_green
+
+# Frete do Seu João
+delivery_time_line1 = DeliveryTimeLine.create!(init_distance: 1, final_distance: 100, delivery_time: 2,
+                                               delivery_time_table:
+                                               DeliveryTimeTable.find_by(shipping_company: inactive_shipping_company1))
+print '.'.green
+delivery_time_line2 = DeliveryTimeLine.create!(init_distance: 101, final_distance: 200, delivery_time: 4,
+                                               delivery_time_table:
+                                               DeliveryTimeTable.find_by(shipping_company: inactive_shipping_company1))
+print '.'.green
+# Frete do Seu Meireles
+delivery_time_line3 = DeliveryTimeLine.create!(init_distance: 1, final_distance: 50, delivery_time: 2,
+                                               delivery_time_table:
+                                               DeliveryTimeTable.find_by(shipping_company: inactive_shipping_company2))
+print '.'.green
+delivery_time_line4 = DeliveryTimeLine.create!(init_distance: 51, final_distance: 100, delivery_time: 4,
+                                               delivery_time_table:
+                                               DeliveryTimeTable.find_by(shipping_company: inactive_shipping_company2))
+print '.'.green
+# Frete da Dona Catarina
+delivery_time_line5 = DeliveryTimeLine.create!(init_distance: 1, final_distance: 60, delivery_time: 1,
+                                               delivery_time_table:
+                                               DeliveryTimeTable.find_by(shipping_company: active_shipping_company1))
+print '.'.green
+delivery_time_line6 = DeliveryTimeLine.create!(init_distance: 61, final_distance: 120, delivery_time: 3,
+                                               delivery_time_table:
+                                               DeliveryTimeTable.find_by(shipping_company: active_shipping_company1))
+print '.'.green
+# Frete da Dona Paula
+delivery_time_line7 = DeliveryTimeLine.create!(init_distance: 1, final_distance: 120, delivery_time: 1,
+                                               delivery_time_table:
+                                               DeliveryTimeTable.find_by(shipping_company: active_shipping_company2))
+print '.'.green
+delivery_time_line8 = DeliveryTimeLine.create!(init_distance: 121, final_distance: 240, delivery_time: 6,
+                                               delivery_time_table:
+                                               DeliveryTimeTable.find_by(shipping_company: active_shipping_company2))
+print ". DONE\n".green
+
+puts 'creating => vehicles'.bg_green
+
+# Frete do Seu João
+vehicle1 = Vehicle.create!(plate: '8585-POU', brand_model: 'Volksvagem - Delivery 9.170', year: '2022',
+                           weight_capacity: 8800, shipping_company: inactive_shipping_company1)
+print '.'.green
+vehicle2 = Vehicle.create!(plate: '7894-HGU', brand_model: 'Volksvagem - Charger 5.600', year: '2021',
+                           weight_capacity: 11_000, shipping_company: inactive_shipping_company1)
+print '.'.green
+# Frete do Seu Meireles
+vehicle3 = Vehicle.create!(plate: '4679-KJL', brand_model: 'Mercedes - CountryMan', year: '2018',
+                           weight_capacity: 9400, shipping_company: inactive_shipping_company2)
+print '.'.green
+vehicle4 = Vehicle.create!(plate: '4948-FLS', brand_model: 'Scania - Evolution', year: '2010',
+                           weight_capacity: 8500, shipping_company: inactive_shipping_company2)
+print '.'.green
+# Frete da Dona Catarina
+vehicle5 = Vehicle.create!(plate: '3156-GSD', brand_model: 'VMAG - 7077', year: '1987',
+                           weight_capacity: 7500, shipping_company: active_shipping_company1)
+print '.'.green
+vehicle6 = Vehicle.create!(plate: '7256-MHL', brand_model: 'Mercedes - Scrambler', year: '2023',
+                           weight_capacity: 13_000, shipping_company: active_shipping_company1)
+print '.'.green
+# Frete da Dona Paula
+vehicle7 = Vehicle.create!(plate: '4913-ALF', brand_model: 'Audi - Discovery', year: '2022',
+                           weight_capacity: 12_000, shipping_company: active_shipping_company2)
+print '.'.green
+vehicle8 = Vehicle.create!(plate: '0302-GLO', brand_model: 'Volksvagem - Trooper 7.667', year: '2023',
+                           weight_capacity: 12_500, shipping_company: active_shipping_company2)
 print ". DONE\n".green
